d8242247c5b6ebc201a2b062e078e0dc3afa61cb
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-09-30 20:42:49 +0700

    change board into 2d

--- lib/Board.rb
+++ lib/Board.rb
@@ -1,24 +1,50 @@
 class Board
-  attr_reader :board
+  attr_reader :width
+
   def initialize
-    @board = Array.new(9)
+    @width  = 3
+    @board = Array.new(width) { Array.new(width) }
   end
 
-  def mark(position:, marker:)
-    return unless in_range?(position)
+  def mark(row, col)
+    raise "Invalid coord" unless valid_coord(row, col)
 
-    board[position] = marker
+    board[row][col] = marker
   end
-  if in_range?(position)
 
-  def size
-    9
+  def valid_coord(row, col)
+    row.match(/^\d+$/) && col.match(/^\d+$/) && in_range(row: row, col: col)
   end
+
+  def same_symbol?(*positions)
+    positions.all { |position| board[position] && board[position] == board[0]}
+  end
+
+  def avaiable_positions
+    board.select { |position| position.nil? }
+  endchoice
+
+  def any_line_match?
+    CONDITIONS = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+    CONDITIONS.any? { |condition| same_symbol?(condition) }
+  end
+
 
   private
-  attr_writer :board
+  
+  attr_accessor :board
 
-  def in_range?(position)
-    position.between(0...size)
+  def in_range?(row, col)
+    row.between(0, width - 1) && column.between(0, width - 1)
   end
 end
