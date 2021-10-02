5339c9e120d14f13035c3824c9b03c2ebacf5fad
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-10-02 20:11:32 +0700

    made win conditions tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,25 +5,35 @@ describe Game do
   describe 'win conditions' do
     let(:game_win) { described_class.new(board: Board.new(board: board_array)) }
 
-    before do
-      allow(game_top_row).to receive(:introduction)
-    end
-
-    context "when board's top row match" do
+    context 'when a row match' do
       let(:board_array) { ['x', 'x', 'x', 'o', 'o', nil, nil, nil, nil] }
 
-      it 'calls announce_winner' do
-        expect(game_win).to receive(:announce_winner).once
-        game_win.start_game
+      it 'is game over' do
+        expect(game_win).to be_game_over
       end
     end
 
-    context "when board's mid row match" do
-      let(:board_array) { [nil, nil, nil, 'o', 'o', 'o', nil, nil, nil] }
+    context 'when a column match' do
+      let(:board_array) { ['x', 'x', 'o', 'x', 'o', 'o', 'x', nil, nil] }
 
-      it 'calls announce_winner' do
-        expect(game_win).to receive(:announce_winner).once
-        game_win.start_game
+      it 'is game over' do
+        expect(game_win).to be_game_over
+      end
+    end
+
+    context 'when a cross match' do
+      let(:board_array) { ['o', 'x', 'x', 'x', 'o', 'o', 'x', nil, 'o'] }
+
+      it 'is game over' do
+        expect(game_win).to be_game_over
+      end
+    end
+
+    context 'no line match' do
+      let(:board_array) { ['x', 'x', 'o', 'x', 'o', 'o', nil, 'x', nil] }
+
+      it 'is not game over' do
+        expect(game_win).to_not be_game_over
       end
     end
   end
@@ -57,6 +67,46 @@ describe Game do
       it 'calls game_start twice' do
         expect(game_start).to receive(:play_round).twice
         game_start.start_game
+      end
+    end
+  end
+
+  describe '#check_end_case' do
+    context 'when board.any_line_match? returns true' do
+      let(:board) { instance_double(Board) }
+      subject(:game_check) { described_class.new(board: board) }
+
+      before do
+        allow(board).to receive(:any_line_match?).and_return(true)
+      end
+
+      it 'calls announce_winner' do
+        expect(game_check).to receive(:announce_winner).once
+        game_check.check_end_case
+      end
+    end
+
+    context 'when board.any_line_match? returns false' do
+      let(:board) { instance_double(Board) }
+      subject(:game_check) { described_class.new(board: board) }
+
+      before do
+        allow(board).to receive(:any_line_match?).and_return(false)
+      end
+
+      it 'calls announce_tie' do
+        expect(game_check).to receive(:announce_tie).once
+        game_check.check_end_case
+      end
+    end
+  end
+
+  describe '#switch_active_player' do
+    context 'when current_active_player_index is 0' do
+      subject(:game_switch) { described_class.new(current_active_player_index: 0) }
+
+      it 'changes to 1' do
+        expect { game_switch.switch_active_player }.to change { }
       end
     end
   end
